b30de0d4b7732c6522befaa7b125fd21cc26313d
Vinod Kumar <vinodkumar@192.168.1.2>
2025-07-07 12:33:21 +0530

    renamed:    src/main/java/com/example/spring_boot_modules/SpringBootModulesApplication.java -> src/main/java/com/spring_boot_modules/SpringBootModulesApplication.java 	renamed:    src/test/java/com/example/spring_boot_modules/SpringBootModulesApplicationTests.java -> src/test/java/com/spring_boot_modules/SpringBootModulesApplicationTests.java

--- src/main/java/com/spring_boot_modules/SpringBootModulesApplication.java
+++ src/main/java/com/spring_boot_modules/SpringBootModulesApplication.java
@@ -1,4 +1,4 @@
-package com.example.spring_boot_modules;
+package com.spring_boot_modules;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
